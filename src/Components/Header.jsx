e0078d5499c782fdff62b5ff5003dfba800f8b84
Zuleika Nyx Delfin <zdelfin@ISBXs-MacBook-Pro.local>
2022-08-05 14:35:42 +0800

    TECH-213: Update SubHeader Components Part2

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import reactLogo from '../assets/react.svg';
+import React from "react";
+import reactLogo from "../assets/react.svg";
 
-export default function Header(){
-return (
-  <div className="nav">
-   <img className="logo" src={reactLogo} alt="React logo" />
-   <div className="headerTitle">
-   <h3 className="whiteTitle">TOUR</h3>
-    <h3 className="grayTitle">OF</h3>
-    <h3 className="blueTitle">HEROES</h3>
-   </div>
-    
-  </div>
-);
+export default function Header() {
+  return (
+    <div className="nav">
+      <img className="logo" src={reactLogo} alt="React logo" />
+      <div className="headerTitle">
+        <h3 className="whiteTitle">TOUR</h3>
+        <h3 className="grayTitle">OF</h3>
+        <h3 className="blueTitle">HEROES</h3>
+      </div>
+    </div>
+  );
 }
