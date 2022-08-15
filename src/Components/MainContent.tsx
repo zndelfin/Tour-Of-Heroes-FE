720415582c705a4f387b7c2479736c0cdb9b1d6c
Zuleika Nyx Delfin <zdelfin@isbx.com>
2022-08-15 11:38:08 +0800

    TECH-215: Removed unused dependencies and added styled-components

--- src/Components/MainContent.tsx
+++ src/Components/MainContent.tsx
@@ -2,10 +2,10 @@ import Menu from "./Menu";
 import { Box } from "@mui/system";
 import Title from "./Title";
 import CardComponent from "./CardComponent";
-
+import styled from "styled-components";
 
 export default function MainContent() {
-  
+
   return (
     <Box sx={{ display: "flex", justifyContent: "space-around" }}>
       {/* MENU COMPONENT */}
