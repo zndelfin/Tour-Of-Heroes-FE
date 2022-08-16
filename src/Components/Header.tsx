14f97c3bf4cee122dd74a16fb46bc77de637dbc5
Zuleika Nyx Delfin <zdelfin@isbx.com>
2022-08-16 11:19:04 +0800

    TECH-215: Update version tooltip

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -3,6 +3,9 @@ import reactLogo from "../assets/react.svg";
 export default function Header() {
   return (
     <div className="nav">
+      <div className="tooltip"> .
+        <span className="tooltiptext">v0816</span>
+      </div>
       <img className="logo" src={reactLogo} alt="React logo" />
       <div className="headerTitle">
         <h3 className="whiteTitle">TOUR</h3>
