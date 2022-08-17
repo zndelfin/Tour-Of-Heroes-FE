6a44aa441b7f2166eac52856e018a0474d582663
Zuleika Nyx Delfin <zdelfin@isbx.com>
2022-08-17 09:37:56 +0800

    TECH-215: Import of version from package json

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,19 +1,18 @@
 import reactLogo from "../assets/react.svg";
+import { version } from "/package.json";
 
 function Header() {
   return (
     <div className="nav">
-      <div className="tooltip"> .
-        <span className="tooltiptext">v0816 3:31PM</span>
-      </div>
       <img className="logo" src={reactLogo} alt="React logo" />
       <div className="headerTitle">
         <h3 className="whiteTitle">TOUR</h3>
         <h3 className="grayTitle">OF</h3>
         <h3 className="blueTitle">HEROES</h3>
       </div>
+      {console.log(version)}
     </div>
   );
 }
 
-export {Header};
+export { Header };
