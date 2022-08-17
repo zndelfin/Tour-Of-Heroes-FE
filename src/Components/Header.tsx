35b77e7c2a9be24b734e010cb988e99eaea53796
Zuleika Nyx Delfin <zdelfin@isbx.com>
2022-08-17 09:42:57 +0800

    TECH-215: Update Header component to add version

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -4,15 +4,18 @@ import { version } from "/package.json";
 function Header() {
   return (
     <div className="nav">
+      <div className="tooltip"> .
+        <span className="tooltiptext">{version}</span>
+      </div>
       <img className="logo" src={reactLogo} alt="React logo" />
       <div className="headerTitle">
         <h3 className="whiteTitle">TOUR</h3>
         <h3 className="grayTitle">OF</h3>
         <h3 className="blueTitle">HEROES</h3>
       </div>
-      {console.log(version)}
+    {console.log(version)}
     </div>
   );
 }
 
-export { Header };
+export {Header};
