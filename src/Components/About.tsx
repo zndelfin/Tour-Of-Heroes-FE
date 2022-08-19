3db630b1d9b8cf310bdd5ba4433079c296ae3b12
Zuleika Nyx Delfin <zdelfin@isbx.com>
2022-08-19 08:55:11 +0800

    TECH-218: About Page

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -1,10 +1,36 @@
+import { Box, CardContent, Typography } from "@mui/material";
+import styled from "styled-components";
 
-function About(){
-    return (
-        <div>
-        <h1>ABOUT</h1>
-        </div>
-    )
+const StyledTypography = styled(Typography)`
+variant: body1;
+justify-content: center;
+text-align: center;
+`;
+
+function About() {
+const currentYear = new Date().getFullYear();
+console.log(currentYear);
+
+  return (
+    <Box m={4} sx={{display:"flex", flexDirection:"column", justifyContent:"center"}}>
+      <StyledTypography variant="h4">ABOUT US</StyledTypography>
+
+      <Box m={5}>
+      <StyledTypography>
+        We're on a mission to make it easier to know your Heroes and identify
+        the Villains.
+      </StyledTypography>
+
+      <StyledTypography>
+        Feel free to add in your heroes! Villains are welcome, too.
+      </StyledTypography>
+
+      
+
+      <StyledTypography m={5}> © {currentYear}</StyledTypography>
+      </Box>
+    </Box>
+  );
 }
 
 export { About };
