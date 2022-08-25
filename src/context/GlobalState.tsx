0e9677d063002a3629155d583ae24f0547a3d7d4
Zuleika Nyx Delfin <zdelfin@isbx.com>
2022-08-25 12:14:06 +0800

    TECH-216: Implemented POST, PATCH and DELETE method from FE to NEST

--- src/context/GlobalState.tsx
+++ src/context/GlobalState.tsx
@@ -1,3 +1,4 @@
+import { ConsoleLogger } from "@nestjs/common";
 import { createContext, useState, useEffect } from "react";
 import { v4 as uuid } from "uuid";
 
@@ -5,6 +6,8 @@ import { v4 as uuid } from "uuid";
 export const GlobalContext = createContext({});
 
 const GlobalContextProvider = (props : any) => {
+
+const [characters, setCharacters] : any = useState([]);
 
 const apiURL = "http://localhost:3000/characters/";
 
@@ -20,20 +23,52 @@ useEffect(() => {
 }, [])
 
 
-const [characters, setCharacters] : any = useState([])
-
 
 const addCharacter = (name: any, description: any) => {
+
+  fetch(apiURL, {
+    method: "POST",
+    body: JSON.stringify({
+      name,
+      description
+    }),
+    headers: {
+    "Content-type": "application/json; charset=UTF-8"
+    }
+  })
+  .then(response => response.json())
+  .then(json => console.log(json))
+
   setCharacters([...characters, {id:uuid(), name, description}]) 
 }
 
 const deleteCharacter = (id: number) => {
+
+  fetch(`http://localhost:3000/characters/${id}`, 
+  {
+    method: "DELETE"
+  })
+
   setCharacters(characters.filter((character: { id: number; }) => id !== character.id))
 }
 
 const editCharacter = (id: number, updatedCharacter: any) => {
   setCharacters(characters.map((character: { id: number; }) => 
   character.id === id ? updatedCharacter : character))
+
+  fetch(`http://localhost:3000/characters/${id}`, {
+    method: "PATCH",
+    body: JSON.stringify({
+      id: updatedCharacter.id,
+      name: updatedCharacter.name,
+      description: updatedCharacter.description
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  })
+  .then(response => response.json())
+  .then(json => console.log(json))
 }
 
   return (
